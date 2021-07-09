ee39d6d18ba9f60c7e9d3de82648a34a4920aeca
damari <motokin128@gmail.com>
2021-07-10 02:44:35 +0900

    商品の選択処理を実装

--- main.rb
+++ main.rb
@@ -1,14 +1,29 @@
 require 'csv'
 class Customer
+  # customers.csv からユーザーデータを読み込み
   def initialize
     @lists = []
     CSV.foreach("customers.csv", headers: true) do |row|
       @lists << {id: row["id"].to_i, name: row["name"], point: row["point"].to_i, password: row["password"]}
     end
   end
+
+  def choose_product(products)
+    # 商品番号の入力（複数可）
+    while true
+      print "ご購入の商品番号を入力してください。 > "
+      product_num = gets.split(' ').map(&:to_i)
+      if product_num.any?
+        break if product_num.all? { |num| num.between?(1, products.length) }
+      end
+      puts "【！】入力した商品番号はありません。"
+    end
+    product_num
+  end
 end
 
 class Shop
+  # products.csv から商品データを読み込み
   def initialize
     @products = []
     CSV.foreach("products.csv", headers: true) do |row|
@@ -17,6 +32,7 @@ class Shop
   end
 
   def disp_products
+    # 商品を一覧表示
     puts "  ＜商品一覧＞"
     @products.each.with_index(1) do |product, i|
       puts "#{i}:#{product[:name]} : #{product[:price]}円"
@@ -26,4 +42,5 @@ end
 
 customer = Customer.new
 shop = Shop.new
-shop.disp_products
+products = shop.disp_products
+customer.choose_product(products)
